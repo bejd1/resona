d27f42eae6a24fa768f699000e31f174a517ba1e
Filip <bejdi7@gmail.com>
2024-01-05 23:15:26 +0100

    create register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,10 +1,10 @@
-import { CardWithForm } from "@/components/loginPage";
+import { CardWithFormRegister } from "@/components/registerPage";
 import React from "react";
 
 const Login = () => {
   return (
     <div className="flex space-x-6 h-[82vh] px-0 sm:px-12 lg:px-24 py-16 items-center justify-center">
-      <CardWithForm />
+      <CardWithFormRegister />
     </div>
   );
 };
--- src/components/registerPage.tsx
+++ src/components/registerPage.tsx
@@ -10,16 +10,14 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Button } from "./ui/button";
 import Link from "next/link";
+import { Button } from "./ui/button";
 
-export function CardWithForm() {
+export function CardWithFormRegister() {
   return (
     <Card className="w-5/6 sm:w-[420px] mb-32">
       <CardHeader>
-        <CardTitle className="text-center text-2xl">
-          Log Into My Account
-        </CardTitle>
+        <CardTitle className="text-center text-2xl">Start</CardTitle>
         <CardDescription>
           <Input className="my-4 h-12" placeholder="Login" />
           <Input className="h-12" type="password" placeholder="Password" />
@@ -27,14 +25,17 @@ export function CardWithForm() {
       </CardHeader>
       <CardFooter>
         <div className="flex flex-col items-center gap-1 w-full">
-          <Link href="/register">
-            <p className="underline mb-2">Don't have an account?</p>
-          </Link>
+          <div className="flex gap-1">
+            <p className="mb-2">Have an account?</p>
+            <Link href="/login">
+              <span className="underline">Sign In</span>
+            </Link>
+          </div>
           <button className="bg-black px-4 py-3 font-poppins text-white rounded-md hover:text-gray-400 ease-in-out duration-300 w-full">
-            SIGN UP
+            CREATE ACCOUNT
           </button>
           <p>or</p>
-          <Button>
+          <Button className="bg-black hover:bg-black">
             <FaGithub />
           </Button>
         </div>
