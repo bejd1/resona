fe9f95afe38a3d5237e9f994e5e3e1a07a452427
Filip <bejdi7@gmail.com>
2024-01-07 16:57:10 +0100

    install prisma and connect with neon db

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -2,13 +2,41 @@
 
 import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
+import { db } from "../../lib/db";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  // Validate input using the RegisterSchema
   const validateFields = RegisterSchema.safeParse(values);
 
+  // Check if validation was successful
   if (!validateFields.success) {
     return { error: "Invalid fields" };
   }
 
-  return { success: "Fine" };
+  const { name, email, password } = validateFields.data;
+
+  // Hash the password using bcrypt
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // Check if the user with the provided email already exists
+  const existingUser = await db.user.findUnique({
+    where: { email },
+  });
+
+  // If user already exists, return an error
+  if (existingUser) {
+    return { error: "Email already in use" };
+  }
+
+  // Create a new user in the database
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  return { success: "Registration successful" };
 };
