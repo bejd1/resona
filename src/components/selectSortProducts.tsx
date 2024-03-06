72be3e342ad0d804b8cd170fea3dfe6da9bad7b1
Filip <bejdi7@gmail.com>
2024-03-06 01:52:23 +0100

    Repair - select value in products

--- src/components/selectSortProducts.tsx
+++ src/components/selectSortProducts.tsx
@@ -7,7 +7,7 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Button, MenuItem } from "@mui/material";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 interface productsButtonI {
   name: string;
@@ -21,17 +21,28 @@ const SelectSortProducts = ({
 }) => {
   const router = useRouter();
   const [isOpen, setIsOpen] = useState(false);
-  const [category, setCategory] = useState("");
+  const searchParams = useSearchParams();
+
+  const category = searchParams.get("category");
 
   const handleClick = (href: string, name: string) => {
     setIsOpen(false);
-    setCategory(name);
     router.push(`/products?category=${href}`);
   };
 
   const handleSelectTriggerClick = () => {
     setIsOpen(!isOpen);
   };
+
+  function setSelectValue(category: string | null) {
+    if (!category || category === "") {
+      return "All";
+    } else if (category === "wireless-earbuds") {
+      return "Wireless Earbuds";
+    } else {
+      return category.charAt(0).toUpperCase() + category.slice(1);
+    }
+  }
 
   return (
     <div>
@@ -40,7 +51,7 @@ const SelectSortProducts = ({
           className="w-full sm:w-[180px]"
           onClick={handleSelectTriggerClick}
         >
-          <SelectValue placeholder={`${category}`} />
+          <SelectValue placeholder={setSelectValue(category)} />
         </SelectTrigger>
         {isOpen && (
           <SelectContent>
